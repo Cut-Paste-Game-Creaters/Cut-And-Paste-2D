48bf6cd26ef9b97f0fe27f1704edac21a799427b
Otakeru0408 <83808363+Otakeru0408@users.noreply.github.com>
2025-05-25 19:59:38 +0900

    タイトルシーンとステージセレクトシーンを作成
    作成したもの
    ・タイトルシーン（適当）
    ・ステージセレクトシーン
    ・プレイヤーの動きを処理するPlayer_Moveスクリプトを作成
    Player_Moveはジャンプにこだわったから見てほしい

---
+++ Assets/Scripts/Player/Player_Move.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    /*
+     注意すること
+    ・動きに関するものはPlayerInput.deltaTimeを使用する。
+    ・入力もPlayerInputを使用する。
+    ・物理演算(rigidbodyなど)を使うときは必ずFixedUpdate
+    ・入力と物理演算の処理は分ける。
+    ・TimeScaleを0にすると、Updateは普通に動くがFixedUpdateは動かなくなる
+     */
+
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float groundCheckDistance = 0.1f;
+    [SerializeField] float maxHoldTime = 0.2f;          // 追加ジャンプ力を加える最大時間（秒）
+    [SerializeField] LayerMask groundLayer;
+
+    private Rigidbody2D rb; 
+    private bool isJumping = false;
+    private float jumpTimeCounter;
+    private float gravity_init = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gravity_init = rb.gravityScale;
+        Time.fixedDeltaTime = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.transform.position;
+        //プレイヤーの左右の動き
+        if (PlayerInput.GetKey(KeyCode.A))
+        {
+            pos.x -= moveSpeed * PlayerInput.GetDeltaTime();
+        }
+        if (PlayerInput.GetKey(KeyCode.D))
+        {
+            pos.x += moveSpeed * PlayerInput.GetDeltaTime();
+        }
+
+        //プレイヤーのジャンプ
+
+        // 接地していて Wキーを押した瞬間にジャンプ開始
+        if (IsGrounded() && PlayerInput.GetKeyDown(KeyCode.W))
+        {
+            isJumping = true;
+            jumpTimeCounter = maxHoldTime;
+        }
+
+        // Wキーを離したらジャンプ終了
+        if (PlayerInput.GetKeyUp(KeyCode.W))
+        {
+            isJumping = false;
+        }
+
+        this.transform.position = pos;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                jumpTimeCounter -= PlayerInput.GetFixedDeltaTime();
+            }
+            else
+            {
+                isJumping = false; // 時間切れで終了
+            }
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = gravity_init;
+        }
+        else
+        {
+            rb.gravityScale = gravity_init;
+        }
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, groundCheckDistance, groundLayer);
+        Debug.DrawRay(this.transform.position, Vector2.down * groundCheckDistance, Color.red);
+        return hit.collider != null;
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerInput
+{
+    public static bool isPausing = false;
+
+    public static float GetDeltaTime()
+    {
+        return Time.deltaTime;
+    }
+
+    public static float GetFixedDeltaTime()
+    {
+        return Time.fixedDeltaTime;
+    }
+    public static bool GetKeyDown(KeyCode key)
+    {
+        if (isPausing) return false;
+        return Input.GetKeyDown(key);
+    }
+
+    public static bool GetKeyDown1(KeyCode key)
+    {
+        return Input.GetKeyDown(key);
+    }
+
+    public static bool GetKey(KeyCode key)
+    {
+        if (isPausing) return false;
+        return Input.GetKey(key);
+    }
+
+    public static bool GetKeyUp(KeyCode key)
+    {
+        if (isPausing) return false;
+        return Input.GetKeyUp(key);
+    }
+
+    public static bool GetMouseButtonDown(int i)
+    {
+        if (isPausing) return false;
+        return Input.GetMouseButtonDown(i);
+    }
+
+    public static bool GetMouseButtonDown1(int i)
+    {
+        return Input.GetMouseButtonDown(i);
+    }
+
+    public static bool GetMouseButton(int i)
+    {
+        if (isPausing) return false;
+        return Input.GetMouseButton(i);
+    }
+
+    public static bool GetMouseButtonUp(int i)
+    {
+        if (isPausing) return false;
+        return Input.GetMouseButtonUp(i);
+    }
+}
+
+
+
+/*
+ if(Input.GetKeyDown(KeyCode.A)){
+    ...
+}
+ 
+ */
