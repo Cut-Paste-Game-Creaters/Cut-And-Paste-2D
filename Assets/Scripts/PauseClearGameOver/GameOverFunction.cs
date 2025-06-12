b1c3df2018308f40ad40b378e7d423cc83abbaed
strayyy441 <gomasio1214@gmail.com>
2025-06-12 18:22:45 +0900

    PlayerFuctionとGameOverFunction機能の追加（未完成）

---
+++ Assets/Scripts/PauseClearGameOver/GameOverFunction.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverFunction : MonoBehaviour
+{
+    [SerializeField] Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position =
+            new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z + 1);
+    }
+
+    public void GameOver()
+    {
+        canvas.enabled = true;
+        PlayerInput.isPausing = true;
+        Time.timeScale = 0f;
+    }
+
+    public void PauseOff()
+    {
+        canvas.enabled = false;
+        PlayerInput.isPausing = false;
+        Time.timeScale = 1.0f;
+    }
+}
