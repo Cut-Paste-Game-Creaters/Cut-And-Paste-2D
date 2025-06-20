295e4ad56c1f71c908e2c55ed7e599365ccbbacd
Otakeru0408 <83808363+Otakeru0408@users.noreply.github.com>
2025-06-20 16:18:08 +0900

    UIをつけた at StageTemplete

---
+++ Assets/Scripts/UI/GameUIController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameUIController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text_HP;
+    [SerializeField] TextMeshProUGUI text_nowCost;
+    [SerializeField] TextMeshProUGUI text_allCost;
+
+    private StageManager stageManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        stageManager = FindObjectOfType<StageManager>();
+        text_HP.text = "PlayerHP\n:" + stageManager.player_HP;
+        text_nowCost.text = "nowCost\n:" + stageManager.have_ene;
+        text_allCost.text = "allCost\n:" + stageManager.all_sum_cos;
+    }
+}
