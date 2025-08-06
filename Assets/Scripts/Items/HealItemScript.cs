95b4842e448a041dc41c766673531bcfc8a2b18a
Effect777-ww <123effect321@gmail.com>
2025-08-07 00:47:16 +0900

    Revert "GameUI変更"
    This reverts commit ae534aca82f404e37084765042b3a4148ce7382e.

--- Assets/Scripts/Items/HealItemScript.cs
+++ Assets/Scripts/Items/HealItemScript.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class HealItemScript : MonoBehaviour
 {
+    private StageManager stageManager;
     private int Healamount = 20;
 
     void Start()
     {
-        // もう StageManager の参照取得は不要なので空でOK
+        stageManager = FindObjectOfType<StageManager>();
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // プレイヤーにぶつかったときのみ処理（必要なら条件追加）
-        if (StageManager.Instance == null) return;
-
         SEManager.instance.ClipAtPointSE(SEManager.instance.healSE);
-
-        if (StageManager.Instance.player_HP + Healamount <= StageManager.Instance.player_MAXHP)
+        if (stageManager.player_HP + Healamount <= stageManager.player_MAXHP)
         {
-            StageManager.Instance.player_HP += Healamount;
-            Debug.Log(Healamount + " 回復");
+            stageManager.player_HP += Healamount;
+            Debug.Log(Healamount + "回復");
+
         }
         else
         {
-            StageManager.Instance.player_HP = StageManager.Instance.player_MAXHP;
+            stageManager.player_HP = stageManager.player_MAXHP;
         }
+
 
         Destroy(this.gameObject);
     }
