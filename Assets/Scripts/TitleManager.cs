4b4874ce2d29fd6f9408b6842efa69ab4801aa9c
Otakeru0408 <83808363+Otakeru0408@users.noreply.github.com>
2025-08-07 18:29:07 +0900

    title画面を設定した。画面押下でページが開くだけ

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,12 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    public void OnGameStart()
+    [SerializeField] GameObject noteBook;
+    [SerializeField] float rotSpeed = 1.0f;
+    [SerializeField] Sprite frontImage;
+    [SerializeField] Sprite backImage;
+
+    private RectTransform noteTransform;
+    private bool isAnimStart = false;
+    private Image noteImage;
+
+    void Start()
     {
-        SceneManager.LoadScene("StageSelectScene");
+        noteTransform = noteBook.GetComponent<RectTransform>();
+        noteImage = noteBook.transform.GetChild(0).GetComponent<Image>();
+        noteImage.sprite=frontImage;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            noteTransform.localEulerAngles = new Vector3(0, 0, 0);
+            isAnimStart = true;
+        }
+
+        if(isAnimStart)
+        {
+            noteTransform.localEulerAngles = new Vector3(0, noteTransform.localEulerAngles.y + rotSpeed, 0);
+            if(noteTransform.localEulerAngles.y > 90.0f)
+            {
+                noteImage.sprite = backImage;
+            }
+            else
+            {
+                noteImage.sprite = frontImage;
+            }
+            if (noteTransform.localEulerAngles.y > 180.0f)
+            {
+                isAnimStart = false;
+            }
+            Debug.Log("rotY:" + noteTransform.localEulerAngles.y);
+        }
     }
 }
