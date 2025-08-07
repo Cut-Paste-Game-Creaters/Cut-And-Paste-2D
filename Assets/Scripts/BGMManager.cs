2de499afedb9e1ab0a77365ab3c0a40c7856c454
Otakeru0408 <83808363+Otakeru0408@users.noreply.github.com>
2025-08-07 14:27:21 +0900

    BGM設定できるようにした

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -1,24 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
 
 public class BGMManager : MonoBehaviour
 {
+    [System.Serializable] // Unityエディタでシリアライズ可能にする
+    public class Pair<T1, T2>
+    {
+        public T1 first;
+        public T2 second;
+
+        public Pair(T1 first, T2 second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
     public static BGMManager instance;
+
+    [SerializeField] private Pair<AudioClip, AudioClip> stageSelectSounds;
+    [SerializeField] private Pair<AudioClip, AudioClip>[] sceneSounds;
 
     private AudioSource ambientsound;
     private AudioSource mainBGM;
 
-    //環境音
-    public AudioClip bard;
-    public AudioClip calm;
-    public AudioClip park;
-    public AudioClip semi;
+    public Dictionary<string, int> stageNumber = new Dictionary<string, int>() // Dictionaryクラスの宣言と初期値の設定
+    {
+        {"Stage1", 0},
+        {"Stage2", 1},
+        {"Stage3", 2},
+        {"Stage4", 3},
+        {"Stage5", 4},
+        {"Stage6", 5},
+        {"Stage7", 6},
+        {"Stage8", 7},
+        {"Stage9", 8},
+        {"Stage10", 9},
+        {"StageTemplate",10 }
+    };
 
-    //BGM
-    public AudioClip groundStage;
-    public AudioClip catsleStage;
-    public AudioClip cosmicStage;
     /// <summary>
     /// scene開始時にBGMと環境音を設定
     /// scene遷移する際はStopAllBGM
@@ -41,6 +64,28 @@ public class BGMManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "StageSelectScene")
+        {
+            PlayDualBGM(stageSelectSounds.first, stageSelectSounds.second);
+        }
+    }
+
+    public void OnSceneLoaded(Scene nextScene, LoadSceneMode mode)
+    {
+        StopAllBGM();
+        if(SceneManager.GetActiveScene().name == "StageSelectScene")
+        {
+            PlayDualBGM(stageSelectSounds.first,stageSelectSounds.second);
+        }
+        else if (Regex.IsMatch(SceneManager.GetActiveScene().name, @"^Stage\d+$")) //シーン名がStageなんとかなら
+        {
+            int stage_num = stageNumber[SceneManager.GetActiveScene().name];
+            PlayDualBGM(sceneSounds[stage_num].first, sceneSounds[stage_num].second);
         }
     }
 
